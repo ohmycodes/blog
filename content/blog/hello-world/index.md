8abe98ffc6c67936ba2d00876caedf02e1b637d9
maurofokker <mgaldames@gmail.com>
2019-01-11 00:51:48 -0300

    first commit creating gatsbyjs blog starter

---
+++ content/blog/hello-world/index.md
@@ -0,0 +1,8 @@
+---
+title: Hello World
+date: '2019-01-10T23:00:00.000Z'
+---
+
+This is my first post on my new blog! Oh My Code! using _GatsbyJS_ :)
+
+I'll write here all my notes that I have in my notebooks
---
+++ src/components/Bio.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Image from 'gatsby-image'
+import { FaTwitter } from 'react-icons/fa'
+
+import { rhythm } from '../utils/typography'
+
+function Bio() {
+  return (
+    <StaticQuery
+      query={bioQuery}
+      render={data => {
+        const { author, social } = data.site.siteMetadata
+        return (
+          <div
+            style={{
+              display: `flex`,
+              marginBottom: rhythm(2.5),
+            }}
+          >
+            <Image
+              fixed={data.avatar.childImageSharp.fixed}
+              alt={author}
+              style={{
+                marginRight: rhythm(1 / 2),
+                marginBottom: 0,
+                minWidth: 50,
+                borderRadius: `100%`,
+              }}
+              imgStyle={{
+                borderRadius: `50%`,
+              }}
+            />
+            <p>
+              Written by <strong>{author}</strong> and friends.
+              {` `}
+              <a href={`https://twitter.com/${social.twitter}`}>
+                <FaTwitter />
+                {` `}
+                Follow him on Twitter
+              </a>
+            </p>
+          </div>
+        )
+      }}
+    />
+  )
+}
+
+const bioQuery = graphql`
+  query BioQuery {
+    avatar: file(absolutePath: { regex: "/hqdefault.jpg/" }) {
+      childImageSharp {
+        fixed(width: 50, height: 50) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        author
+        social {
+          twitter
+        }
+      }
+    }
+  }
+`
+
+export default Bio
